d8f6f412c2ca6e0cade2761b3c1da7a139c21aca
Thiago Souto <thiagocsouto@outlook.com>
2022-06-24 09:43:19 -0400

    Delete bliblioteca/bliblioteca directory

--- bliblioteca/bliblioteca/src/main/resources/schema-h2.sql
+++
@@ -1,5 +0,0 @@
-create table autor(id integer auto_increment, nome varchar(60), email varchar(40), descrição varchar(400)); 
-
-create table cliente(id integer auto_increment, nome varchar(60), cpf varchar(15) ,email varchar(40)); 
-
-create table livro(id integer auto_increment, título varchar(50), resumo varchar(100) , preço varchar(10), página varchar(5), categoria varchar(40)); 
